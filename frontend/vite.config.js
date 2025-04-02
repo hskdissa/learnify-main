f2489cbed84dc7aebc3189bfc2eba72592e0117e
Apawam03 <hskdissa@liverpool.ac.uk>
2025-04-02 18:28:50 +0100

    fixed routing for backend with Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,15 @@ import react from '@vitejs/plugin-react';
 // Vitest configuration
 export default defineConfig({
   server: {
-    port: 5001
+    port: 3000,
+    // Added proxy for the backend api, so that it knows which port our backend is running on
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   },
   plugins: [react()],
   test: {
